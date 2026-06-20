dcde1fa71aaa3b6ad018476e56dc73f662ec4034
AlexCh <alex.shaginyan0507@gmail.com>
2026-06-20 09:15:54 +0200

    Securite: mot de passe minimum 8 caracteres a l'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -49,7 +49,7 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Veuillez entrer un mot de passe',
                     ]),
                     new Length(
-                        min: 6,
+                        min: 8,
                         max: 4096,
                         minMessage: 'Votre mot de passe doit contenir au moins {{ limit }} caractères'
                     ),
